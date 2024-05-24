8f8cf4ff9eea777236f3cb58e78a9c285a68a33d
Jonathan Carvalheiro <138172090+JhowDevc@users.noreply.github.com>
2024-05-24 13:43:56 -0300

    Add files via upload
    Controller's criadas e alteradas

---
+++ desafioDuxus-main/desafioDuxus-main/src/main/java/br/com/duxusdesafio/controller/IntegranteController.java
@@ -0,0 +1,202 @@
+package br.com.duxusdesafio.controller;
+
+import br.com.duxusdesafio.config.ModelMapperCf;
+import br.com.duxusdesafio.model.domain.Integrante;
+import br.com.duxusdesafio.model.domain.Time;
+import br.com.duxusdesafio.model.input.IntegranteInput;
+import br.com.duxusdesafio.model.view.IntegranteView;
+import br.com.duxusdesafio.model.view.TimeView;
+import br.com.duxusdesafio.service.IntegranteService;
+import br.com.duxusdesafio.service.TimeService;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.http2.HpackDecoder;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Integrante controlador
+ */
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/integrante")
+public class IntegranteController {
+    private final IntegranteService itService;
+    private final TimeService tmService;
+    private final ModelMapperCf modelMapper;
+
+    /**
+     * Busca todos os integrantes na base de dados
+     *
+     * @return Todas os integrantes achados
+     */
+    @ResponseBody
+    @GetMapping("/")
+    public ResponseEntity<List<IntegranteView>> findAll() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(itService.findAll());
+    }
+
+    /**
+     * Busca um unico integrante na base de dados com base no identificador dado
+     *
+     * @param id Itentificador unico
+     * @return Integrante
+     */
+    @ResponseBody
+    @GetMapping("/{id}")
+    public ResponseEntity<IntegranteView> findById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(itService.findById(id));
+    }
+
+    /**
+     * Salva um novo integrante na base de dados
+     *
+     * @param itInput Integrante a ser salvo
+     * @return Integrante salvo
+     */
+    @ResponseBody
+    @PostMapping("/")
+    public ResponseEntity<IntegranteView> saveOne(@RequestBody IntegranteInput itInput) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(itService.saveOne(itInput));
+    }
+
+    /**
+     * Atualiza um integrante da base de dados com base no identificador dado
+     *
+     * @param id Identificador unico
+     * @param itInput Integrante com as devidas alteracoes
+     * @return Status code No Content
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity updateOne(@PathVariable Long id, @RequestBody IntegranteInput itInput) {
+        itService.updateOne(id, itInput);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    /**
+     * Deleta um integrante da base de dados com base no idetificador dado
+     *
+     * @param id Identificador unico
+     * @return Status code No Content
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteOne(@PathVariable Long id) {
+        itService.deleteOne(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    /**
+     * Busca integrante mais usado nas composicoes
+     *
+     * @param data1 Data de inicio
+     * @param data2 Data final
+     * @return Integrante
+     */
+    @ResponseBody
+    @GetMapping("it-mais-usado")
+    public ResponseEntity<IntegranteView> integranteMaisUsadoOp(
+            @RequestParam("data1") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate data1,
+            @RequestParam("data2") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate data2) {
+        List<Time> tms = modelMapper.mapList(tmService.findAll(), Time.class);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(modelMapper.map(itService.integranteMaisUsado(
+                        data1,
+                        data2,
+                        tms
+                ), IntegranteView.class));
+    }
+
+    /**
+     * Busca nome de todos os integrantes nas composicoes do periodo
+     *
+     * @param data1 Data de inicio
+     * @param data2 Data final
+     * @return Lista de Strings
+     */
+    @ResponseBody
+    @GetMapping("its-nome")
+    public ResponseEntity<List<String>> timeMaisComumOp(
+            @RequestParam("data1") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate data1,
+            @RequestParam("data2") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate data2) {
+        List<Time> tms = modelMapper.mapList(tmService.findAll(), Time.class);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(itService.timeMaisComum(data1, data2, tms));
+    }
+
+    /**
+     * Busca funcao mais comum das composicoes
+     *
+     * @param data1 Data de inicio
+     * @param data2 Data final
+     * @return String
+     */
+    @ResponseBody
+    @GetMapping("funcao-mais-comum")
+    public ResponseEntity<String> funcaoMaisComum(
+            @RequestParam("data1") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate data1,
+            @RequestParam("data2") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate data2) {
+        List<Time> tms = modelMapper.mapList(tmService.findAll(), Time.class);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(itService.funcaoMaisComum(data1, data2, tms));
+    }
+
+    /**
+     * Busca franquia mais famosa das composicoes
+     *
+     * @param data1 Data de inicio
+     * @param data2 Data final
+     * @return String
+     */
+    @ResponseBody
+    @GetMapping("franquia-mais-famosa")
+    public ResponseEntity<String> franquiaMaisFamosa(
+            @RequestParam("data1") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate data1,
+            @RequestParam("data2") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate data2) {
+        List<Time> tms = modelMapper.mapList(tmService.findAll(), Time.class);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(itService.franquiaMaisFamosa(data1, data2, tms));
+    }
+
+    /**
+     * Busca a franquia mais famosa e sua contagem
+     *
+     * @param data1 Data de inicio
+     * @param data2 Data final
+     * @return HashMap com contagem de ocorrencias
+     */
+    @ResponseBody
+    @GetMapping("contagem-franquia")
+    public ResponseEntity<Map<String, Long>> contagemPorFranquia(
+            @RequestParam("data1") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate data1,
+            @RequestParam("data2") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate data2) {
+        List<Time> tms = modelMapper.mapList(tmService.findAll(), Time.class);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(itService.contagemPorFranquia(data1, data2, tms));
+    }
+
+    /**
+     * Busca a funcao mais famosa e sua contagem
+     *
+     * @param data1 Data de inicio
+     * @param data2 Data final
+     * @return HashMap com contagem de ocorrencias
+     */
+    @ResponseBody
+    @GetMapping("contagem-funcao")
+    public ResponseEntity<Map<String, Long>> contagemPorFuncao(
+            @RequestParam("data1") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate data1,
+            @RequestParam("data2") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate data2) {
+        List<Time> tms = modelMapper.mapList(tmService.findAll(), Time.class);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(itService.contagemPorFuncao(data1, data2, tms));
+    }
+}
--- desafioDuxus-main/desafioDuxus-main/src/main/java/br/com/duxusdesafio/controller/TimeController.java
+++ desafioDuxus-main/desafioDuxus-main/src/main/java/br/com/duxusdesafio/controller/TimeController.java
@@ -1,87 +1,104 @@
-package br.com.duxusdesafio.controller;
+package br.com.duxusdesafio.controller.rest;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-import javax.validation.Valid;
-
-import br.com.duxusdesafio.model.Time;
-import br.com.duxusdesafio.repository.TimeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import br.com.duxusdesafio.config.ModelMapperCf;
+import br.com.duxusdesafio.model.domain.Time;
+import br.com.duxusdesafio.model.input.TimeInput;
+import br.com.duxusdesafio.model.view.TimeView;
+import br.com.duxusdesafio.service.TimeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.List;
 
-
+/**
+ * Time controlador
+ */
+@RequiredArgsConstructor
 @RestController
-@RequestMapping(path="/times")
+@RequestMapping("/api/v1/time")
 public class TimeController {
+    private final TimeService tmService;
+    private final ModelMapperCf modelMapper;
 
-    @Autowired
-    private TimeRepository timeRepository;
-
-
-    @GetMapping
-    public ResponseEntity<List<Time>> listAll(){
-        List<Time> times = new ArrayList<>();
-        times = timeRepository.findAll();
-        return ResponseEntity.ok(times);
+    /**
+     * Busca todos os times na base de dados
+     *
+     * @return Todas os times achados
+     */
+    @ResponseBody
+    @GetMapping("/")
+    public ResponseEntity<List<TimeView>> findAll() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(tmService.findAll());
     }
 
-//    @GetMapping
-//    @Cacheable(value = "listaTimes")
-//    public ResponseEntity<Page<Time>> listar(Pageable pageable){
-//        var times = timeRepository;
-//        return new ResponseEntity<>(times, HttpStatus.OK);
-//    }
+    /**
+     * Busca um unico time na base de dados com base no identificador dado
+     *
+     * @param id Itentificador unico
+     * @return Time
+     */
+    @ResponseBody
+    @GetMapping("/{id}")
+    public ResponseEntity<TimeView> findById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(tmService.findById(id));
+    }
 
-//    @PostMapping
-//    @CacheEvict(value = "listaTimes")
-//    public ResponseEntity<Time> create(@Valid @RequestBody Time time){
-//        timeRepository.save(time);
-//        return new ResponseEntity<>(time, HttpStatus.OK);
-//    }
-//
-//
-//    @GetMapping("/{id}")
-//    public ResponseEntity<Time> findById(@PathVariable Long id) {
-//        Optional<Time> time = timeRepository.findById(id);
-//        return time
-//                .map(prod -> ResponseEntity.ok().body(prod))
-//                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
-//    }
-//
-//
-//    @GetMapping("/buscarPorNome")
-//    public ResponseEntity<List<TimeDto>> listByNome(String q){
-//        List<Time> times = new ArrayList<>();
-//        times = timeRepository.findByNomeContainingIgnoreCase(q);
-//        return ResponseEntity.ok(TimeDto.converte(times));
-//    }
-//
-//    @PutMapping("/{id}")
-//    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Time time) {
-//        return timeRepository.findById(id).map(tim -> {
-//
-//            tim.setNome(time.getNome());
-//
-//
-//
-//            Time timeAtualizado = timeRepository.save(time);
-//            return ResponseEntity.status(HttpStatus.OK).body(timeAtualizado);
-//
-//        }).orElse(ResponseEntity.notFound().build());
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<?> deleteById(@PathVariable Long id) {
-//        return timeRepository.findById(id).map(prod -> {
-//            timeRepository.deleteById(id);
-//            return ResponseEntity.noContent().build();
-//        }).orElse(ResponseEntity.notFound().build());
-//    }
+    /**
+     * Salva um novo time na base de dados
+     *
+     * @param tmInput Time a ser salvo
+     * @return Time salvo
+     */
+    @ResponseBody
+    @PostMapping("/")
+    public ResponseEntity<TimeView> saveOne(@RequestBody TimeInput tmInput) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(tmService.saveOne(tmInput));
+    }
 
+    /**
+     * Atualiza um time da base de dados com base no identificador dado
+     *
+     * @param id Identificador unico
+     * @param tmInput Time com as devidas alteracoes
+     * @return Status code No Content
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity updateOne(@PathVariable Long id, @RequestBody TimeInput tmInput) {
+        tmService.updateOne(id, tmInput);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    /**
+     * Deleta um time da base de dados com base no idetificador dado
+     *
+     * @param id Identificador unico
+     * @return Status code No Content
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteOne(@PathVariable Long id) {
+        tmService.deleteOne(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    /**
+     * Busca o primeiro time com data correspondente a data dada
+     *
+     * @param data Data
+     * @return Time
+     */
+    @ResponseBody
+    @GetMapping("/time-da-data")
+    public ResponseEntity<TimeView> timeDaDataOp(@RequestParam("data")
+                                                 @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate data) {
+        List<Time> tms = modelMapper.mapList(tmService.findAll(), Time.class);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(modelMapper.map(tmService.timeDaData(data, tms), TimeView.class));
+    }
 }
